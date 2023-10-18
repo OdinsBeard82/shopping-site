29bd6c9905ab473bb3e59fe4df74345559b03cf1
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-10-18 16:09:57 +0100

    products images and hover done

--- script.js
+++ script.js
@@ -1,62 +1,3 @@
-const shoppingCart = []; 
-
-const addToCart = (productId, productName, price) => {
-  const existingItem = shoppingCart.find(item => item.productId === productId);
-
-  if (existingItem) {
-    existingItem.quantity++;
-  } else {
-    shoppingCart.push({
-      productId,
-      productName,
-      price,
-      quantity: 1
-    });
-  }
-
-  updateCartDisplay();
-}
-
-const removeFromCart = (productId) => {
-  const productIndex = shoppingCart.findIndex(item => item.productId === productId);
-
-  if (productIndex !== -1) {
-    shoppingCart.splice(productIndex, 1);
-  }
-
-  updateCartDisplay();
-}
-
-const calculateTotalPrice = () => {
-  const totalPrice = shoppingCart.reduce((total, item) => {
-    return total + item.price * item.quantity;
-  }, 0);
-
-  document.getElementById("total-price").textContent = `$${totalPrice.toFixed(2)}`;
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const addToCartButtons = document.querySelectorAll(".add-to-cart-button");
-  addToCartButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const productId = button.getAttribute("data-product-id");
-      const productName = button.getAttribute("data-product-name");
-      const price = parseFloat(button.getAttribute("data-product-price"));
-      addToCart(productId, productName, price);
-      calculateTotalPrice();
-    });
-  });
-
-  const removeFromCartButtons = document.querySelectorAll(".remove-from-cart-button");
-  removeFromCartButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const productId = button.getAttribute("data-product-id");
-      removeFromCart(productId);
-      calculateTotalPrice();
-    });
-  });
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   var chairElements = document.querySelectorAll(".product2");
 
@@ -66,3 +7,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
