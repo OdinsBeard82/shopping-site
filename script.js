f17e74f0782448b50b03733203822280028ad7bc
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-10-18 15:34:54 +0100

    added click to all images

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  var chairElements = document.querySelectorAll(".chair");
+  var chairElements = document.querySelectorAll(".product2");
 
   chairElements.forEach(function (chairElement) {
     chairElement.addEventListener("click", function () {
