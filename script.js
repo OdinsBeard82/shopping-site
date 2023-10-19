153b0a4c19561e07be1f3d710b15f0535b318e70
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-10-19 16:57:26 +0100

    working on getting items into cart

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { doc } = require("prettier");
+
 document.addEventListener("DOMContentLoaded", function () {
   var chairElements = document.querySelectorAll(".product2");
 
@@ -6,5 +8,18 @@ document.addEventListener("DOMContentLoaded", function () {
       window.location = "./products.html";
     });
   });
-});
 
+  document.addEventListener("DOMContentLoaded", function () {
+    var cartElements = document.querySelectorAll(".addToCart");
+    
+    cartElements.forEach(function (cartElement) {
+      cartElement.addEventListener("click", function () {
+        let img = document.createElement("img");
+        img.src = cartElement.getAttribute("data-element-imageurl"); 
+  
+        let cart = document.getElementById("cart");
+        cart.appendChild(img);
+      });
+    });
+  });
+});
